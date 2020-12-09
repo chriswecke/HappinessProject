46fdd50a4ab43110198dd7c625581e3db7a89850
Jeff Lyng <jely94@icloud.com>
2020-12-08 18:47:55 -0600

    added legend to map

--- static/anychart.js
+++ static/anychart.js
@@ -5,24 +5,24 @@ anychart.onDocumentReady(function () {
         console.log(data)
 
         // Variable to hold the data
-        var geoData = data 
+        var geoData = data
 
         // // create a new array with the resulting data
         var data = [];
 
         // Go through the initial data
         for (var i = 0; i < geoData.length; i++) {
-            
+
             // insert the resulting data in the array using the AnyChart keywords 
             data.push({ id: geoData[i].ISO2, value: geoData[i].Happiness_Score, title: geoData[i].Country })
-            
+
         };
 
         console.log(data)
 
         // connect the data with the map
         var chart = anychart.map(data);
-        
+
         chart.geoData(anychart.maps.world);
 
         // specify the chart type and set the series 
@@ -33,13 +33,13 @@ anychart.onDocumentReady(function () {
 
         // color scale ranges
         ocs = anychart.scales.ordinalColor([
-            { less: 3.99 },
-            { from: 4, to: 4.49 },
-            { from: 4.5, to: 4.99 },
-            { from: 5, to: 5.49 },
-            { from: 5.50, to: 5.99 },
-            { from: 6, to: 6.49 },
-            { from: 6.50, to: 6.99 },
+            { less: 3.999 },
+            { from: 4, to: 4.499 },
+            { from: 4.5, to: 4.999 },
+            { from: 5, to: 5.499 },
+            { from: 5.50, to: 5.999 },
+            { from: 6, to: 6.499 },
+            { from: 6.50, to: 6.999 },
             { greater: 7 }
         ]);
 
@@ -49,11 +49,25 @@ anychart.onDocumentReady(function () {
         // tell the series what to use as a colorRange (colorScale)
         series.colorScale(ocs);
 
+        // enable the legend
+        chart.legend(true);
+
+        // set the source mode of the legend and add styles
+        chart.legend()
+            .itemsSourceMode("categories")
+            .position('right')
+            .align('top')
+            .itemsLayout('vertical')
+            .padding(50, 0, 0, 20)
+            .paginator(false);
+            
+
         // set the container id
         chart.container('container');
 
         // draw the chart
         chart.draw();
     });
+
 
 });
